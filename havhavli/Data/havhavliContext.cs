65fc5933992d46c8da6c1685e27851fcfdea7150
Tomer Mantzuri <tomermantzuri@gmail.com>
2021-08-18 15:22:32 +0300

    Add project files.

---
+++ havhavli/Data/havhavliContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using havhavli.Models;
+
+namespace havhavli.Data
+{
+    public class havhavliContext : DbContext
+    {
+        public havhavliContext (DbContextOptions<havhavliContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<havhavli.Models.Product> Product { get; set; }
+
+        public DbSet<havhavli.Models.Category> Category { get; set; }
+    }
+}
---
+++ havhavli/Migrations/20210818073424_init.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace havhavli.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SerialNumber",
+                table: "Product",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Product",
+                newName: "SerialNumber");
+        }
+    }
+}
