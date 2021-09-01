1fad51f94e006ed64665f9392067bb5caa2d8f1d
Tomer Mantzuri <tomermantzuri@gmail.com>
2021-09-01 10:53:36 +0300

    2.2 small update

--- havhavli/Migrations/20210901075036_Images.Designer.cs
+++ havhavli/Migrations/20210901075036_Images.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using havhavli.Data;
 
 namespace havhavli.Migrations
 {
     [DbContext(typeof(havhavliContext))]
-    partial class havhavliContextModelSnapshot : ModelSnapshot
+    [Migration("20210901075036_Images")]
+    partial class Images
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
