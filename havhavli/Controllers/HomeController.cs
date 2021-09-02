4b37c33619e57cefd1dd3299743689514890e90b
Tomer Mantzuri <tomermantzuri@gmail.com>
2021-09-02 14:58:50 +0300

    Developers

--- havhavli/Controllers/HomeController.cs
+++ havhavli/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace havhavli.Controllers
         {
             return View();
         }
-        public IActionResult Contact()
+        public IActionResult Developers()
         {
             return View();
         }
